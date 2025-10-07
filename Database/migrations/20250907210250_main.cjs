a5fdef59410c821b354a18b9ec5baa095a9640e0
CodeAnomaly08 <CodeAnomaly08@gmail.com>
2025-10-07 04:59:02 -0300

    Revert "Merge branch 'Manutencao'"
    This reverts commit 77cb377224608ee9de3b7f53740619411b3a4109, reversing
    changes made to e018da5b0a85bbee5c51e3672be1967db956dce4.

--- Database/migrations/20250907210250_main.cjs
+++ Database/migrations/20250907210250_main.cjs
@@ -2,8 +2,9 @@
  * @param { import("knex").Knex } knex
  * @returns { Promise<void> }
  */
-exports.up = function(knex) {
- return knex.schema.createTable('setores',table=>{
+exports.up = async function(knex) {
+ await knex.schema
+ .createTable('setores',table=>{
   table.increments('ID').primary().notNullable();
   table.string('name').notNullable().defaultTo('Novo Setor');
  })
@@ -26,15 +27,47 @@ exports.up = function(knex) {
   table.float('valor').notNullable().defaultTo(0);
   table.string('customData').defaultTo('');
  })
+ .createTable('users',table=>{
+  table.increments('UID').primary().notNullable();
+  table.string('nome').notNullable();
+  table.string('email').notNullable();
+  table.string('senha').notNullable();
+  table.integer('rank').defaultTo(1);
+ })
+ .createTable('useRegister',table=>{
+  table.increments('ID').primary().notNullable();
+  table.string('user').notNullable();
+  table.string('useremail').notNullable();
+  table.string('acao').notNullable();
+  table.dateTime('data').notNullable();
+ })
+ .createTable('manutencaoLinks',table=>{
+  table.increments('ID').primary();
+  table.integer('objetoID').notNullable();
+  table.integer('manutencaoID').notNullable();
+ })
+ .createTable('manutencoes',table=>{
+  table.increments('ID').primary();
+  table.boolean('concluido').defaultTo(false);
+  table.date('data').notNullable();
+  table.date('retornoEsperado').notNullable();
+  table.float('valor').notNullable();
+  table.float('tipoObjetoID').notNullable();
+  });
 };
+
 /**
  * @param { import("knex").Knex } knex
  * @returns { Promise<void> }
  */
-exports.down = function(knex) {
- return knex.schema
+exports.down = async function(knex) {
+  return knex.schema
+  .dropTable('users')
   .dropTable('objetos')
   .dropTable('tipoObjetos')
   .dropTable('subSetores')
   .dropTable('setores')
+  .dropTable('useRegister')
+  .dropTable('manutencoes')
+  .dropTable('manutencaoLinks')
 };
--- Database/routes/manutencoes.js
+++ Database/routes/manutencoes.js
@@ -4,9 +4,8 @@ import { Router } from "express";
 
 const router = Router();
 
-router.get('/manutencoes/:old',authMiddleware,rankMiddleware(4),async(req,res)=>{
- const {old} = req.params;
- const mainarray = await database('manutencoes').where('concluido',old==1);
+router.get('/manutencoes',authMiddleware,rankMiddleware(4),async(req,res)=>{
+ const mainarray = await database('manutencoes').where('concluido',false);
 
  if (mainarray.length<1)
   return res.status(200).json({ok:true,result:
@@ -22,11 +21,6 @@ router.get('/manutencoes/:old',authMiddleware,rankMiddleware(4),async(req,res)=>
   linkMap.set(m.ID,links.filter(l=>l.manutencaoID==m.ID));
  });
 
- const typeMap = new Map();
- for (const type of await database('tipoObjetos').whereIn('ID',[...new Set(mainarray.map(m=>m.tipoObjetoID))])){
-  typeMap.set(type.ID,type)
- }
-
  const objMap = new Map();
  for (const obj of await database('objetos').whereIn('ID',[...new Set(links.map(l=>l.objetoID))]))
  {
@@ -41,7 +35,8 @@ router.get('/manutencoes/:old',authMiddleware,rankMiddleware(4),async(req,res)=>
  const mains = [];
  mainarray.forEach(main=>{
   mainCount++;
-  const mainObj = {...main,objType:typeMap.get(main.tipoObjetoID).name,sumValue:0,objects:[]};
+  const mainObj = {...main,sumValue:0,objects:[]};
+  mains.push(mainObj);
   linkMap.get(main.ID).forEach(l=>{
    const obj = objMap.get(l.objetoID)
    if (!obj)return;
@@ -53,7 +48,6 @@ router.get('/manutencoes/:old',authMiddleware,rankMiddleware(4),async(req,res)=>
    }
    mainObj.objects.push(obj);
   });
-  mains.push(mainObj);
  });
  
  return res.status(200).json({ok:true,result:
@@ -64,28 +58,16 @@ router.get('/manutencoes/:old',authMiddleware,rankMiddleware(4),async(req,res)=>
   }});
 });
 
-
-router.put('/manutencoes',authMiddleware,rankMiddleware(4),async(req,res)=>{
- const {manID} = req.body;
- await database.transaction(async trx=>{
-  await trx('manutencoes').update({concluido:true}).where('ID',manID);
-  for (const link of await database('manutencaoLinks').where('manutencaoID',manID).transacting(trx)){
-   await trx('objetos').update({estado:0}).where('ID',link.objetoID);
-  }
- });
- return res.status(200).json({ok:true});
-})
-
-
 router.post('/manutencoes',authMiddleware,rankMiddleware(4),async(req,res)=>{
- const {objIDs,value,data,calldate,desc} = req.body;
+ const {objIDs,value,data} = req.body;
+ console.log(data);
  const objects = await database('objetos').whereIn('ID',objIDs);
  for (const item of objects){
   if (item.estado==1)
    return res.status(400).json({ok:false,error:'Objeto(s) já em manutenção!'})
  }
  await database.transaction(async trx=>{
-  const main = (await trx('manutencoes').insert({descricao:desc,valor:value,retornoEsperado:new Date(data),data:new Date(calldate),tipoObjetoID:objects[0].tid}).returning('*'))[0]
+  const main = (await trx('manutencoes').insert({valor:value,retornoEsperado:new Date(data),data:new Date(),tipoObjetoID:objects[0].tid}).returning('*'))[0]
   await trx('manutencaoLinks').insert(objIDs.map(o=>({objetoID:o,manutencaoID:main.ID})))
   for (const id of objIDs){
    await trx('objetos').update({estado:1}).where('ID',id);
